1ebd30a192102369730bb6d8b1c671fcbb4546b9
Puneeth Chaganti <punchagan@muse-amuse.in>
2021-11-01 20:27:27 +0530

    Add deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+EXPORT_DIR="out"
+
+# Build the site
+pushd $(dirname $0)/..
+yarn build
+yarn export
+
+# Push to muse-amuse.in
+rsync -azP "${EXPORT_DIR}/" muse-amuse.in:~/artful-dodger.muse-amuse.in
+
+popd
