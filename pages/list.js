b91b0ecb0b753158f952c31f97e1ca9ad29eeead
Puneeth Chaganti <punchagan@muse-amuse.in>
2021-11-13 15:24:09 +0530

    Use Google Drive thumbnail links instead of uc links
    The uc links stop working after a few reloads because of some kind of
    rate-limiting on "automated queries" by Google
    
    https://support.google.com/websearch/answer/86640

--- pages/list.js
+++ pages/list.js
@@ -62,7 +62,8 @@ const Entries = ({ metadataUrl }) => {
     data.map((row, idx) => (
       <div key={idx}>
         {row.title}
-        <img src={`https://drive.google.com/uc?id=${row.thumbnail}&export=view`} />
+        <img src={`https://drive.google.com/thumbnail?id=${row.thumbnail}`} />
+        {/* <img src={`https://lh3.googleusercontent.com/d/${row.thumbnail}=s1980`} /> */}
         {row.description}
       </div>
     ))
