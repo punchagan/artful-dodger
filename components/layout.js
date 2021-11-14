6fc5a4757df41049df4dbb945bbfbd916e12b279
Puneeth Chaganti <punchagan@muse-amuse.in>
2021-11-14 11:45:30 +0530

    Use antd Layout and Menu to improve the page header, navbar

--- components/layout.js
+++ components/layout.js
@@ -1,32 +1,33 @@
 import Link from "next/link";
 import Head from "next/head";
 import "antd/dist/antd.css";
+import { Layout, Menu } from "antd";
+const { Header, Content, Footer } = Layout;
 
 export default function BaseLayout({ children, title = "This is the default title" }) {
   return (
-    <div>
+    <>
       <Head>
         <title>The Artful Dodger</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <header>
-        <Link href="/">
-          <a>The Artful Dodger</a>
-        </Link>
-        <nav>
-          <Link href="/about">
-            <a>About</a>
-          </Link>
-          <Link href="/list">
-            <a>Gallery</a>
-          </Link>
-        </nav>
-      </header>
-
-      {children}
-
-      <footer></footer>
-    </div>
+      <Layout>
+        <Header>
+          <Menu mode="horizontal">
+            <Menu.Item key="1">
+              <Link href="/">The Artful Dodger</Link>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Link href="/about">About</Link>
+            </Menu.Item>
+            <Menu.Item key="3">
+              <Link href="/list">Gallery</Link>
+            </Menu.Item>
+          </Menu>
+        </Header>
+        <Content>{children}</Content>
+        <Footer></Footer>
+      </Layout>
+    </>
   );
 }
