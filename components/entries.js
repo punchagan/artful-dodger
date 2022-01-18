628e5cb9cc9bb392d5f1ed4e2eaea609bd50dfd2
Puneeth Chaganti <punchagan@muse-amuse.in>
2022-01-18 22:16:37 +0530

    Use basic antd grid for the gallery view

--- components/entries.js
+++ components/entries.js
@@ -1,10 +1,16 @@
+import { Row, Col } from "antd";
+
 export default function Entries({ data }) {
-  return data.map((row, idx) => (
-    <div key={idx}>
-      {row.title}
-      <img src={`https://drive.google.com/thumbnail?id=${row.thumbnail}`} />
-      {/* <img src={`https://lh3.googleusercontent.com/d/${row.thumbnail}=s1980`} /> */}
-      {row.description}
-    </div>
-  ));
+  return (
+    <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+      {data.map((row, idx) => (
+        <Col className="gutter-row" key={idx} span={4}>
+          <div>
+            <img src={`https://drive.google.com/thumbnail?id=${row.thumbnail}`} />
+            {/* <img src={`https://lh3.googleusercontent.com/d/${row.thumbnail}=s1980`} /> */}
+          </div>
+        </Col>
+      ))}
+    </Row>
+  );
 }
