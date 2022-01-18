8fafb6d28aeb6c542d65965ad322a26a74e3f212
Puneeth Chaganti <punchagan@muse-amuse.in>
2022-01-18 23:32:11 +0530

    Make images responsive in gallery and other minor improvements

--- components/entries.js
+++ components/entries.js
@@ -1,14 +1,19 @@
-import { Row, Col } from "antd";
+import { Image, Row, Col } from "antd";
 
 export default function Entries({ data }) {
   return (
-    <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+    <Row
+      justify="center"
+      align="middle"
+      gutter={[
+        { xs: 16, sm: 24, md: 32, lg: 40 },
+        { xs: 16, sm: 24, md: 32, lg: 40 },
+      ]}
+    >
       {data.map((row, idx) => (
-        <Col className="gutter-row" key={idx} span={4}>
-          <div>
-            <img src={`https://drive.google.com/thumbnail?id=${row.thumbnail}`} />
-            {/* <img src={`https://lh3.googleusercontent.com/d/${row.thumbnail}=s1980`} /> */}
-          </div>
+        <Col className="gutter-row" key={idx} span={8}>
+          <Image preview={false} src={`https://drive.google.com/thumbnail?id=${row.thumbnail}`} />
+          {/* <img src={`https://lh3.googleusercontent.com/d/${row.thumbnail}=s1980`} /> */}
         </Col>
       ))}
     </Row>
