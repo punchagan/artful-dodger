72baf5614290afcf51f48d2e8f8d7c5361a85368
Puneeth Chaganti <punchagan@muse-amuse.in>
2022-01-20 01:07:50 +0530

    Fix current page highlight in nav bar

--- components/layout.js
+++ components/layout.js
@@ -1,3 +1,4 @@
+import { useRouter } from "next/router";
 import Link from "next/link";
 import Head from "next/head";
 import "antd/dist/antd.css";
@@ -5,6 +6,18 @@ import { Layout, Menu } from "antd";
 const { Header, Content, Footer } = Layout;
 
 export default function BaseLayout({ children, title = "This is the default title" }) {
+  const router = useRouter();
+  const paths = [
+    { name: "/", title: "The Artful Dodger" },
+    { name: "/viewing-rooms", title: "Viewing Rooms", otherPath: "/room" },
+    { name: "/about", title: "About" },
+  ];
+  const selectedKeys = paths.reduce(
+    (acc, p) =>
+      p.name === router.pathname || p.otherPath === router.pathname ? [...acc, p.name] : acc,
+    []
+  );
+
   return (
     <>
       <Head>
@@ -14,16 +27,12 @@ export default function BaseLayout({ children, title = "This is the default titl
       <Layout>
         <Header>
           <div className="logo" />
-          <Menu theme="dark" mode="horizontal">
-            <Menu.Item key="1">
-              <Link href="/">The Artful Dodger</Link>
-            </Menu.Item>
-            <Menu.Item key="2">
-              <Link href="/viewing-rooms">Viewing Rooms</Link>
-            </Menu.Item>
-            <Menu.Item key="3">
-              <Link href="/about">About</Link>
-            </Menu.Item>
+          <Menu theme="dark" mode="horizontal" selectedKeys={selectedKeys}>
+            {paths.map((path, idx) => (
+              <Menu.Item key={path.name}>
+                <Link href={path.name}>{path.title}</Link>
+              </Menu.Item>
+            ))}
           </Menu>
         </Header>
         <Content style={{ minWidth: "60%" }}>{children}</Content>
