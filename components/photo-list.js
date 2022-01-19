5c5f35226032907e6f5a68f47262015dbc2209d8
Puneeth Chaganti <punchagan@muse-amuse.in>
2022-01-19 17:31:52 +0530

    Make PhotoList component self contained

--- components/photo-list.js
+++ components/photo-list.js
@@ -1,5 +1,9 @@
+import { useState } from "react";
 import { Image, Row, Col } from "antd";
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
+import ReactBnbGallery from "react-bnb-gallery";
+import "react-bnb-gallery/dist/style.css";
+import { LoadingOutlined } from "@ant-design/icons";
 
 // Breakpoint widths taken from https://ant.design/components/layout/
 const columnsCountBreakPoints = {
@@ -8,19 +12,57 @@ const columnsCountBreakPoints = {
   1600: 4, // xxl
 };
 
-export default function PhotoList({ data, openGalleryPhoto }) {
-  return (
-    <ResponsiveMasonry columnsCountBreakPoints={columnsCountBreakPoints}>
-      <Masonry gutter={4}>
-        {data.map((image, idx) => (
-          <Image
-            onClick={() => openGalleryPhoto(idx)}
-            alt={image.caption}
-            preview={false}
-            src={image.thumbnail}
-          />
-        ))}
-      </Masonry>
-    </ResponsiveMasonry>
+export const transformData = (p, number) => {
+  const thumbnail = `https://drive.google.com/thumbnail?id=${p.thumbnail}`;
+  // const photo = `https://lh3.googleusercontent.com/d/${p.thumbnail}=s1980`;
+  const photo = `https://drive.google.com/uc?export=view&id=${p.thumbnail}`;
+  // FIXME: Add sold status?
+  const caption = `${p.title} by ${p.artist}`;
+  const description = p.description || "Excellent work of art";
+  const subcaption = `${description} (Price: ₹ ${p.price})`;
+  return {
+    ...p,
+    photo,
+    thumbnail,
+    number,
+    caption,
+    subcaption,
+  };
+};
+
+export default function PhotoList({ photos, loading }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [activePhotoIndex, setActivePhotoIndex] = useState(0);
+  const openGalleryPhoto = (idx) => {
+    setActivePhotoIndex(idx);
+    setIsOpen(true);
+  };
+
+  return loading ? (
+    <LoadingOutlined />
+  ) : (
+    <>
+      <ResponsiveMasonry columnsCountBreakPoints={columnsCountBreakPoints}>
+        <Masonry gutter={4}>
+          {photos.map((photo, idx) => (
+            <Image
+              onClick={() => openGalleryPhoto(idx)}
+              alt={photo.caption}
+              preview={false}
+              src={photo.thumbnail}
+            />
+          ))}
+        </Masonry>
+      </ResponsiveMasonry>
+      <ReactBnbGallery
+        activePhotoIndex={activePhotoIndex}
+        show={isOpen}
+        photos={photos}
+        onClose={() => setIsOpen(false)}
+        wrap={false}
+        opacity="0.95"
+        backgroundColor="#000000"
+      />
+    </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,17 @@
 import Head from "next/head";
 import BaseLayout from "../components/layout";
-import PhotoList from "../components/photo-list";
+import PhotoList, { transformData } from "../components/photo-list";
 import { useState, useEffect } from "react";
-import ReactBnbGallery from "react-bnb-gallery";
-import "react-bnb-gallery/dist/style.css";
-import { LoadingOutlined } from "@ant-design/icons";
 
 export default function Home({ config }) {
   const [photos, setPhotos] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  const [isOpen, setIsOpen] = useState(false);
-  const [activePhotoIndex, setActivePhotoIndex] = useState(0);
-  const openGalleryPhoto = (idx) => {
-    setActivePhotoIndex(idx);
-    setIsOpen(true);
-  };
-
   useEffect(() => {
     fetch(config.metadataUrl)
       .then((res) => res.json())
       .then((data) => {
-        const photos = data.map((p, number) => {
-          const thumbnail = `https://drive.google.com/thumbnail?id=${p.thumbnail}`;
-          // const photo = `https://lh3.googleusercontent.com/d/${p.thumbnail}=s1980`;
-          const photo = `https://drive.google.com/uc?export=view&id=${p.thumbnail}`;
-          // FIXME: Add sold status?
-          const caption = `${p.title} by ${p.artist}`;
-          const description = p.description || "Excellent work of art";
-          const subcaption = `${description} (Price: ₹ ${p.price})`;
-          return {
-            ...p,
-            photo,
-            thumbnail,
-            number,
-            caption,
-            subcaption,
-          };
-        });
+        const photos = data.map(transformData);
         setPhotos(photos);
         setLoading(false);
       });
@@ -49,17 +23,7 @@ export default function Home({ config }) {
         <h1 className="title">Welcome to The Artful Dodger</h1>
         <p className="description">We consign, commission, buy and sell art.</p>
       </main>
-      {loading && <LoadingOutlined />}
-      <PhotoList data={photos} openGalleryPhoto={openGalleryPhoto} />
-      <ReactBnbGallery
-        activePhotoIndex={activePhotoIndex}
-        show={isOpen}
-        photos={photos}
-        onClose={() => setIsOpen(false)}
-        wrap={false}
-        opacity="0.95"
-        backgroundColor="#000000"
-      />
+      <PhotoList photos={photos} loading={loading} />
     </BaseLayout>
   );
 }
