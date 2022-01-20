7ae4e97f82d169a054d21ff0684a43010a0c5e1a
Puneeth Chaganti <punchagan@muse-amuse.in>
2022-01-20 21:35:08 +0530

    Extract RoomList as a separate component

--- pages/viewing-rooms.js
+++ pages/viewing-rooms.js
@@ -1,61 +1,15 @@
-import Link from "next/link";
 import BaseLayout from "../components/layout";
-import PhotoList, { usePhotos } from "../components/photo-list";
+import { usePhotos } from "../components/photo-list";
+import RoomList from "../components/room-list";
 import { pageStaticProps } from "../lib/page-utils";
-import { tagFilter, tagToTitle } from "../lib/tag-utils";
-import { Image, Row, Col, Card, Avatar, PageHeader, Typography, Space, Spin } from "antd";
+import { PageHeader } from "antd";
 
 export default function Rooms({ config }) {
   const { loading, photos } = usePhotos(config.metadataUrl);
-  const tags = Array.from(new Set(photos.map((x) => x.tags).flat())).sort();
-  const metadata = tags.map((tag) => {
-    const title = tagToTitle(tag);
-    const tagPhotos = photos.filter(tagFilter(tag)).sort((a, b) => a.tags.length - b.tags.length);
-    return tag.length > 0
-      ? { ...tagPhotos[0], title, tag }
-      : {
-          thumbnail: `https://placehold.jp/30/777777/ffffff/300x150.png?text=${title}`,
-          title,
-          tag,
-        };
-  });
-
   return (
     <BaseLayout>
-      <PageHeader
-        title="Viewing Rooms"
-        subTitle="View curated collections of art"
-        backIcon={false}
-      />
-      {loading && <Spin />}
-      <Row
-        justify="center"
-        align="middle"
-        gutter={[
-          { xs: 16, sm: 24, md: 32, lg: 40, xl: 40, xxl: 40 },
-          { xs: 16, sm: 24, md: 32, lg: 40, xl: 40, xxl: 40 },
-        ]}
-      >
-        {metadata.map((photo, idx) => {
-          let href = `/room/?name=${photo.tag}`;
-          return (
-            <Col className="gutter-row" style={{ textAlign: "center" }} key={idx} span={8}>
-              <Card>
-                <Link href={href}>
-                  <Space direction="vertical">
-                    <Avatar
-                      shape="square"
-                      size={{ xs: 128, sm: 128, md: 256, lg: 256, xl: 256, xxl: 256 }}
-                      src={photo.thumbnail}
-                    />
-                    <Typography.Text>{photo.title}</Typography.Text>
-                  </Space>
-                </Link>
-              </Card>
-            </Col>
-          );
-        })}
-      </Row>
+      <PageHeader title="Viewing Rooms" subTitle="View curated collections of art" />
+      <RoomList photos={photos} loading={loading} />
     </BaseLayout>
   );
 }
