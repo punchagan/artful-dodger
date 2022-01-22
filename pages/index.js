1d2a9e673b3018b97c99add2e690fd760fb3b85f
Puneeth Chaganti <punchagan@muse-amuse.in>
2022-01-22 16:26:30 +0530

    Exclude sold art from the landing page

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,15 @@ import { pageStaticProps } from "../lib/page-utils";
 import { PageHeader } from "antd";
 
 export default function Home({ config }) {
+  const excludeSold = (arr) => arr.filter((it) => it.sold !== "y");
   return (
     <BaseLayout>
       <PageHeader title="The Artful Dodger" subTitle="We consign, commission, buy and sell art" />
-      <PhotoList metadataUrl={config.metadataUrl} imagePrefix={config.imagePrefix} />
+      <PhotoList
+        metadataUrl={config.metadataUrl}
+        imagePrefix={config.imagePrefix}
+        transform={excludeSold}
+      />
     </BaseLayout>
   );
 }
