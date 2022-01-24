3c9e46982fa95d85ad76ee1e2487e491edf99d24
Puneeth Chaganti <punchagan@muse-amuse.in>
2022-01-24 18:33:34 +0530

    Format bookmarklet code with prettier

--- scripts/gdrive-url-mapping-bookmarklet.js
+++ scripts/gdrive-url-mapping-bookmarklet.js
@@ -1,18 +1,25 @@
-javascript:void(
-    function(){
-        if (window.location.hostname !== "drive.google.com" ||
-            !window.location.pathname.startsWith('/drive/folders/')) {
-            alert("Not a Google Drive Folder");
-            return;
-        };
-        let csv = Array.from(document.querySelectorAll('.jGNTYb.ACGwFc')).map((el) => [el.getAttribute('aria-label').replace(" Image", ""), el.querySelector('img.l-u-Ab-zb-Ua').src.split('/')[6].split('=')[0]].join(',')).join('\n');
+javascript: void (function () {
+  if (
+    window.location.hostname !== "drive.google.com" ||
+    !window.location.pathname.startsWith("/drive/folders/")
+  ) {
+    alert("Not a Google Drive Folder");
+    return;
+  }
+  let csv = Array.from(document.querySelectorAll(".jGNTYb.ACGwFc"))
+    .map((el) =>
+      [
+        el.getAttribute("aria-label").replace(" Image", ""),
+        el.querySelector("img.l-u-Ab-zb-Ua").src.split("/")[6].split("=")[0],
+      ].join(",")
+    )
+    .join("\n");
 
-        let data = [csv],
-            blob = new Blob(data, {type: 'text/csv'}),
-            a = document.createElement('a');
+  let data = [csv],
+    blob = new Blob(data, { type: "text/csv" }),
+    a = document.createElement("a");
 
-        a.download = 'drive-mapping.csv';
-        a.href = window.URL.createObjectURL(blob);
-        a.click();
-    }()
-);
+  a.download = "drive-mapping.csv";
+  a.href = window.URL.createObjectURL(blob);
+  a.click();
+})();
