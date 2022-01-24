352ef263ce29b1b60284f339e77bbca42238f12e
Puneeth Chaganti <punchagan@muse-amuse.in>
2022-01-24 23:34:30 +0530

    Don't sync images/thumbnails since production uses CDN

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,6 +8,10 @@ pushd $(dirname $0)/..
 yarn build
 yarn export
 
+# Don't sync image/thumbnail dirs since we use CDN on production
+rm -r "${EXPORT_DIR}/image"
+rm -r "${EXPORT_DIR}/thumbnail"
+
 # Push to muse-amuse.in
 rsync -azP "${EXPORT_DIR}/" muse-amuse.in:~/artful-dodger.muse-amuse.in
 
