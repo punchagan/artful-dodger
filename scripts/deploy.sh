f34255d6322fdcb4fc349eb6d3af212b0f5da6ba
Puneeth Chaganti <punchagan@muse-amuse.in>
2022-01-26 13:06:11 +0530

    Sync the download script when deploying the site

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,6 +12,9 @@ yarn export
 rm -r "${EXPORT_DIR}/image"
 rm -r "${EXPORT_DIR}/thumbnail"
 
+# Copy the download script
+cp scripts/download-images "${EXPORT_DIR}"
+
 # Push to muse-amuse.in
 rsync -azP "${EXPORT_DIR}/" muse-amuse.in:~/artful-dodger.muse-amuse.in
 
