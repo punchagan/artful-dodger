81bd540400f428f73b976fcd4ebc416dcce475a8
Puneeth Chaganti <punchagan@muse-amuse.in>
2022-06-16 23:04:01 +0530

    Split rooms list into multiple sections

--- components/room-list.js
+++ components/room-list.js
@@ -9,68 +9,72 @@ const findFirstUnused = (photos, metadata) => {
   return photos.find((it) => !previousThumbnails.has(it.thumbnail));
 };
 
-export const extraRooms = [
-  { title: "Full Collection", id: "all" },
-  { title: "Sold", id: "sold" },
-];
-export default function Rooms({ photos, loading }) {
-  const tags = Array.from(new Set(photos.map((x) => x.tags).flat()))
-    .filter((it) => Boolean(it))
-    .sort();
-  const tagRooms = tags.map((tag) => ({ title: tagToTitle(tag), id: tag }));
-  const rooms = extraRooms.slice(0, 1).concat(tagRooms).concat(extraRooms.slice(1));
+function RoomsSection({ rooms, sectionName, photos, loading }) {
   const metadata = [];
-
-  let meta;
   rooms.forEach((room, idx) => {
-    switch (room.id) {
-      case "all":
-        meta = findFirstUnused(photos, metadata);
-        break;
-
-      case "sold":
-        meta = findFirstUnused(
-          photos.filter((p) => p.sold),
-          metadata
-        );
-        break;
-
-      default:
-        meta = findFirstUnused(photos.filter(tagFilter(room.id)), metadata);
-        break;
-    }
+    const meta = findFirstUnused(photos.filter(room.filter), metadata);
     metadata.push({ ...meta, title: room.title, id: room.id });
   });
 
   return loading ? (
     <Loading />
   ) : (
-    <Row justify="center" align="middle" gutter={[16, 16]}>
-      {metadata.map((room, idx) => {
-        let href = `/room/?name=${room.id}`;
-        return (
-          <Col
-            className="gutter-row"
-            style={{ textAlign: "center" }}
-            key={idx}
-            span={{ xs: 12, sm: 12, md: 12, lg: 8, xl: 8, xxl: 8 }}
-          >
-            <Card bordered={false} hoverable={false}>
-              <Link href={href}>
-                <Space direction="vertical">
-                  <Avatar
-                    style={{ cursor: "pointer" }}
-                    shape="square"
-                    size={{ xs: 96, sm: 128, md: 128, lg: 256, xl: 256, xxl: 256 }}
-                    src={room.thumbnail}
-                  />
-                  <Typography.Text style={{ cursor: "pointer" }}>{room.title}</Typography.Text>
-                </Space>
-              </Link>
-            </Card>
-          </Col>
-        );
-      })}
-    </Row>
+    <div>
+      <h2 style={{ textAlign: "center" }}>{sectionName}</h2>
+      <Row justify="center" align="middle" gutter={[16, 16]}>
+        {metadata.map((room, idx) => {
+          let href = `/room/?name=${room.id}`;
+          return (
+            <Col
+              className="gutter-row"
+              style={{ textAlign: "center" }}
+              key={idx}
+              span={{ xs: 12, sm: 12, md: 12, lg: 8, xl: 8, xxl: 8 }}
+            >
+              <Card bordered={false} hoverable={false}>
+                <Link href={href}>
+                  <Space direction="vertical">
+                    <Avatar
+                      style={{ cursor: "pointer" }}
+                      shape="square"
+                      size={{ xs: 96, sm: 128, md: 128, lg: 256, xl: 256, xxl: 256 }}
+                      src={room.thumbnail}
+                    />
+                    <Typography.Text style={{ cursor: "pointer" }}>{room.title}</Typography.Text>
+                  </Space>
+                </Link>
+              </Card>
+            </Col>
+          );
+        })}
+      </Row>
+    </div>
+  );
+}
+
+// Miscellaneous Rooms
+export const miscRooms = [
+  { title: "Full Collection", id: "all", filter: (p) => p },
+  { title: "Sold", id: "sold", filter: (p) => p.sold },
+];
+
+export default function Rooms({ photos, loading }) {
+  // Tags/Themes rooms
+  const tags = Array.from(new Set(photos.map((x) => x.tags).flat()))
+    .filter((it) => Boolean(it))
+    .sort();
+  const tagsRooms = tags.map((tag) => ({
+    title: tagToTitle(tag),
+    id: tag,
+    filter: tagFilter(tag),
+  }));
+
+  return loading ? (
+    <Loading />
+  ) : (
+    <>
+      <RoomsSection rooms={tagsRooms} photos={photos} sectionName="Themes" />
+      <RoomsSection rooms={miscRooms} photos={photos} sectionName="Miscellaneous" />
+    </>
   );
 }
--- pages/room.js
+++ pages/room.js
@@ -1,7 +1,7 @@
 import { useRouter } from "next/router";
 import BaseLayout from "../components/layout";
 import PhotoList from "../components/photo-list";
-import { extraRooms } from "../components/room-list";
+import { miscRooms } from "../components/room-list";
 import { pageStaticProps } from "../lib/page-utils";
 import { tagFilter, tagToTitle } from "../lib/tag-utils";
 import { PageHeader, Breadcrumb } from "antd";
@@ -9,22 +9,11 @@ import { PageHeader, Breadcrumb } from "antd";
 export default function Room({ config }) {
   const router = useRouter();
   const { name } = router.query;
-  let tagRoom = name ? extraRooms.find((it) => it.id === name) : undefined;
+  let tagRoom = name ? miscRooms.find((it) => it.id === name) : undefined;
   let title = tagRoom ? tagRoom.title : name ? tagToTitle(name) : "";
+  let filter = tagRoom ? tagRoom.filter : tagFilter(name);
   const devEnv = process.env.NODE_ENV !== "production";
   const imagePrefix = devEnv && !config.forceCDN ? "" : config.imagePrefix;
-
-  let filter;
-  switch (name) {
-    case "all":
-      filter = (it) => it;
-      break;
-    case "sold":
-      filter = (it) => it.sold;
-      break;
-    default:
-      filter = tagFilter(name);
-  }
 
   return (
     <BaseLayout siteTitle={config.title} pageTitle={`${title} — ${config.title}`}>
