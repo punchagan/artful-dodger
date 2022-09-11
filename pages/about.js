b3f0bd775b61a7e1caf38f71a39e3da16b21da85
Puneeth Chaganti <punchagan@muse-amuse.in>
2022-09-11 17:25:34 +0530

    Add util to make getStaticProps functions based on md filename

--- pages/about.js
+++ pages/about.js
@@ -2,23 +2,19 @@ import Head from "next/head";
 import BaseLayout from "../components/layout";
 import { Layout } from "antd";
 import { Descriptions } from "antd";
-import { pageStaticProps, getAboutContent } from "../lib/page-utils";
+import { makeGetStaticProps } from "../lib/page-utils";
 
 export default function About({ config }) {
   const { Content } = Layout;
   return (
     <BaseLayout pageTitle={`About — ${config.title}`} siteTitle={config.title}>
       <Content style={{ minWidth: "100%" }}>
-        <div dangerouslySetInnerHTML={{ __html: config.aboutData }} />
+        <div dangerouslySetInnerHTML={{ __html: config.pageData }} />
       </Content>
     </BaseLayout>
   );
 }
 
 export const getStaticProps = async () => {
-  const aboutData = await getAboutContent();
-  const {
-    props: { config },
-  } = await pageStaticProps();
-  return { props: { config: { ...config, aboutData } } };
+  return (await makeGetStaticProps("about.md"))();
 };
