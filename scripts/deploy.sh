43b031b0f42f9f2fab1be7d6f5ccd93c592e97c6
Puneeth Chaganti <punchagan@muse-amuse.in>
2024-07-07 20:53:09 +0530

    Generalise deploy script for any deployments

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 
+CONFIG_DIR=${1:-"production"}
 EXPORT_DIR="out"
+RSYNC_PATH=${2:-"example.com:~/art.example.com"}
 
 pushd $(dirname $0)/..
 
@@ -10,17 +12,18 @@ rm -rf "${EXPORT_DIR}/"
 rm -rf next.config.js
 
 # Copy the env file
-cp production/.env.local.artful-dodger.muse-amuse.in .env.local
-# Download the pages
+cp "${CONFIG_DIR}/.env.local" .env.local
+
+# Remove any old images and thumbnails
+rm -rf "${EXPORT_DIR}/image"
+rm -rf "${EXPORT_DIR}/thumbnail"
+# Download the images and thumbnails
+./scripts/download-images
 ./scripts/download-pages
 
 # Build the site
 yarn build
 yarn export
-
-# Don't sync image/thumbnail dirs since we use CDN on production
-rm -r "${EXPORT_DIR}/image"
-rm -r "${EXPORT_DIR}/thumbnail"
 
 # Copy scripts for cron job
 cp scripts/download-pages "${EXPORT_DIR}"
@@ -28,6 +31,6 @@ cp scripts/download-images "${EXPORT_DIR}"
 cp scripts/update-access-control "${EXPORT_DIR}"
 
 # Push to muse-amuse.in
-rsync -azP "${EXPORT_DIR}/" muse-amuse.in:~/artful-dodger.muse-amuse.in
+rsync -azP "${EXPORT_DIR}/" "${RSYNC_PATH}"
 
 popd
